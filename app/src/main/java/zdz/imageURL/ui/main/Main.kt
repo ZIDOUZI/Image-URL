0dc2864261d40df9db4cffd29d606fc9a2a2375d
子斗子 <1662093634@qq.com>
2023-11-24 17:47:37 +0800

    添加默认动画, 删除无用属性, 统一使用MainNav
    Signed-off-by: 子斗子 <1662093634@qq.com>

--- app/src/main/java/zdz/imageURL/ui/main/Main.kt
+++ app/src/main/java/zdz/imageURL/ui/main/Main.kt
@@ -13,20 +13,19 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.rememberNavController
 import com.ramcosta.composedestinations.DestinationsNavHost
+import com.ramcosta.composedestinations.animations.defaults.RootNavGraphDefaultAnimations
 import com.ramcosta.composedestinations.navigation.dependency
+import com.ramcosta.composedestinations.navigation.navigate
+import com.ramcosta.composedestinations.rememberNavHostEngine
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import zdz.imageURL.BuildConfig
 import zdz.imageURL.R
+import zdz.imageURL.activity.main.MainNav
 import zdz.imageURL.activity.main.MainViewModel
 import zdz.imageURL.ui.main.destinations.Destination
-import zdz.imageURL.ui.main.destinations.HelpDestination
-import zdz.imageURL.ui.main.destinations.HomeDestination
-import zdz.imageURL.ui.main.destinations.LogsDestination
-import zdz.imageURL.ui.main.destinations.SettingsDestination
 import zdz.imageURL.utils.OpenDocumentTree
 import zdz.libs.compose.ex.AlertDialog
 import zdz.libs.compose.ex.icon
@@ -38,8 +37,6 @@ fun Main(vm: MainViewModel, startDestination: Destination, ctx: Context = LocalC
     val launcher = rememberLauncherForActivityResult(contract = OpenDocumentTree) { closure ->
         vm.getRootDir(ctx)?.uri.let { closure(ctx, it) }
     }
-    
-    val query: () -> Unit = { launcher.launch(Unit) }
     
     val scope = rememberCoroutineScope()
     
@@ -64,50 +61,30 @@ fun Main(vm: MainViewModel, startDestination: Destination, ctx: Context = LocalC
     Scaffold(modifier = Modifier.fillMaxSize(), bottomBar = {
         val currentRoute by navCtrl.appCurrentDestinationAsState()
         NavigationBar {
-            entries.forEach { dest ->
-                NavigationBarItem(selected = currentRoute == dest,
-                    onClick = { navCtrl.navigate(dest.route) },
+            MainNav.entries.forEach { (dest, label, icon) ->
+                NavigationBarItem(
+                    selected = currentRoute == dest,
+                    onClick = { navCtrl.navigate(dest) },
                     alwaysShowLabel = true,
-                    icon = dest.icon,
-                    label = { Text(text = dest.label, fontSize = 11.sp) })
+                    label = { Text(text = label) },
+                    icon = icon,
+                )
             }
         }
     }) {
         DestinationsNavHost(
             navGraph = NavGraphs.root,
             modifier = Modifier.padding(it),
+            engine = rememberNavHostEngine(
+                rootDefaultAnimations = RootNavGraphDefaultAnimations.ACCOMPANIST_FADING
+            ),
             navController = navCtrl,
             startRoute = startDestination,
             dependenciesContainerBuilder = {
                 dependency<MainViewModel, _>(NavGraphs.root) { vm }
-                dependency<() -> Unit, _>(NavGraphs.root) { query }
+                dependency<() -> Unit, _>(NavGraphs.root) { { launcher.launch(Unit) } }
                 dependency<suspend () -> Boolean?, _>(NavGraphs.root) { vm::checkUpdate }
             }
         )
     }
 }
-
-val Destination.label: String
-    @Composable
-    get() = when (this) {
-        HelpDestination -> R.string.help.str
-        HomeDestination -> R.string.home.str
-        LogsDestination -> R.string.log.str
-        SettingsDestination -> R.string.settings.str
-    }
-
-val Destination.icon: @Composable () -> Unit
-    @Composable
-    get() = when (this) {
-        HelpDestination -> R.drawable.ic_baseline_help_24.icon
-        HomeDestination -> R.drawable.ic_baseline_home_24.icon
-        LogsDestination -> R.drawable.ic_baseline_history_24.icon
-        SettingsDestination -> R.drawable.ic_baseline_settings_24.icon
-    }
-
-val entries = listOf(
-    HomeDestination,
-    HelpDestination,
-    SettingsDestination,
-    LogsDestination,
-)
